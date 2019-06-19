9c99afd8ebbf78e0b992befef8f33aa0dfd59b86
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-06-19 09:55:24 -0400

    added sign up pages

---
+++ signuppages/script.js
@@ -0,0 +1,19 @@
+// Get the modal
+var modal = document.getElementById('mentorsignup');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+var modal = document.getElementById('menteesignup');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
